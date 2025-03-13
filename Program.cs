035b6ac9fd491a40421c02ffa8898cc5e4eae6fe
pahanexe <paha.oleinik@gmail.com>
2025-03-13 23:31:17 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_lab1
+{
+    internal class Program
+    {
+        void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string filePath = args[0];
+
+                if (File.Exists(filePath))
+                {
+                    Console.WriteLine($"Отримано файл: {filePath}");
+                    // Додаткові дії з файлом, наприклад, зчитування вмісту
+                    string content = File.ReadAllText(filePath);
+                    Console.WriteLine("Вміст файлу:");
+                    Console.WriteLine(content);
+                }
+
+                while (do_stuff()) {
+            }
+
+        }
+
+        private bool do_stuff()
+        {
+
+            return true;
+        }
+    }
+}
